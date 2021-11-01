bc27b3315892d4f28de138f6057c3bced1a3fe31
Manohar Upputuri <46025373+mupputur@users.noreply.github.com>
2021-11-01 19:41:06 +0530

    Add files via upload

---
+++ libCommon/driver_manager.py
@@ -0,0 +1,38 @@
+import os
+from selenium import webdriver
+import time
+
+
+class DriverManager:
+
+    def __init__(self):
+        self.driver = None
+        self.url = "https://opensource-demo.orangehrmlive.com/"
+        self.driver_location = "/home/hariveerapaneni/Downloads/chromedriver"
+        #self.driver_location = "..\\..\\dependecies\\chromedriver.exe"
+        self._initialize_driver()
+
+    def _initialize_driver(self):
+        if not os.path.exists(self.driver_location):
+            raise Exception("Chrome Driver is not present : {}".format(self.driver_location))
+        self.driver = webdriver.Chrome(executable_path=self.driver_location)
+        self.driver.maximize_window()
+        self._launch()
+
+    def _launch(self):
+        self.driver.get(self.url)
+        self.driver.find_element_by_id('txtUsername').send_keys('Admin')
+        self.driver.find_element_by_id('txtPassword').send_keys('admin123')
+        self.driver.find_element_by_id('btnLogin').click()
+
+
+class ChromeDriverManager(DriverManager):
+
+    def __init__(self):
+        pass
+
+
+class FirefoxDriverManger(DriverManager):
+
+    def __init__(self):
+        pass
---
+++ libCommon/selenium_helper.py
@@ -0,0 +1,68 @@
+from selenium.webdriver.support.ui import Select
+
+class SeliniumHelper:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+    def identify_element(self, locator_value, locator_type, element_name):
+        try:
+            if locator_type == "ID":
+                element = self.driver.find_element_by_id(locator_value)
+            elif locator_type == "XPATH":
+                element = self.driver.find_element_by_xpath(locator_value)
+            return element
+        except Exception as e:
+            print("Unable to find the element :{} ERROR: {}".format(element_name, str(e)))
+
+    def click(self, element):
+        try:
+            element.click()
+        except Exception as e:
+            print("Unable to click the element")
+
+    def send_text(self, elem, text):
+        try:
+            elem.send_keys(text)
+        except Exception as e:
+            print("Unable to enter a text")
+
+    def selecting_element(self,elem, visible_text):
+        try:
+
+            drop = (Select(elem)).select_by_visible_text(visible_text)
+            return drop
+        except Exception as e:
+            print("Unable to find the text :{} ERROR: {}".format(visible_text, str(e)))
+
+    def identifying_elements(self, locator_type, locator_value, element_name):
+        try:
+            if locator_type == 'ID':
+                actual_date = self.driver.find_elements_by_xpath(locator_value)
+            elif locator_type == 'XPATH':
+                actual_date = self.driver.find_elements_by_xpath(locator_value)
+                return actual_date
+        except Exception as e:
+            print("Unable to find the elements :{} ERROR: {}".format(element_name, str(e)))
+
+    def claendar_from_date(self,actual_date,select_date):
+        try:
+            for dates in actual_date:
+                date = dates.text
+                if date == select_date:
+                    dates.click()
+                    break
+        except Exception as e:
+            print("Unable to find the element :{} ERROR: {}".format(select_date, str(e)))
+
+
+    def calendar_to_date(self,actual_date,select_date):
+        try:
+            for dates in actual_date:
+                date = dates.text
+                if date == select_date:
+                    dates.click()
+                    break
+        except Exception as e:
+            print("Unable to find the element :{} ERROR: {}".format(select_date, str(e)))
