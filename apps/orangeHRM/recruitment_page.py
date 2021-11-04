7a4e159b95b739300f4892352b1d1ce5bb9fc815
Padmaja-24 <padmaja.g574@gmail.com>
2021-11-04 12:55:58 +0530

    Review mycode

--- apps/orangeHRM/recruitment_page.py
+++ apps/orangeHRM/recruitment_page.py
@@ -1,17 +1,29 @@
-__author__= "Padamja (<git user name>)"
+__author__= "Padamja (<Padmaja-24>)"
+from apps.orangeHRM.home_page import HomePage
+from libCommon.selenium_helper import SeleniumHelper
+
 
 class RecruitmentPage:
-
-    def __init__(self):
-        pass
+    def __init__(self,driver):
+        self.driver = driver
+        home_page = HomePage(self.driver)
+        home_page.navigate_recruitment()
+        self.helper=SeleniumHelper(self.driver)
 
     def naviagte_candidates(self):
-        pass
+        element = self.helper.identify_element('menu_recruitment_viewCandidates', 'ID', 'Candidates')
+        self.helper.click(element)
 
     def navigate_vacancies(self):
-        pass
+        element = self.helper.identify_element('menu_recruitment_viewJobVacancy', 'ID', 'Vacancies')
+        self.helper.click(element)
+
+
 
 if __name__ == "__main__":
-
-    obj = RecruitmentPage()
+    from libCommon.driver_manager import DriverManager
+    driver_mgr = DriverManager()
+    obj = RecruitmentPage(driver_mgr.driver)
+    obj.naviagte_candidates()
     obj.navigate_vacancies()
+
